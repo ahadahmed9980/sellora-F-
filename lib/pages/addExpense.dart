0add24d4b2c218aea36477b82afeffba1f7a4487
ahadahmed9980 <abdul.ahad05112006@gmail.com>
2026-09-25 17:03:55 +0500

    working on expense page

--- lib/pages/addExpense.dart
+++ lib/pages/addExpense.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:sellora/utils/theme/app_theme.dart';
 import 'package:sellora/widgets/appbar.dart';
 import 'package:sellora/widgets/buttons/bottomNavigation.dart';
+import 'package:sellora/widgets/formfield.dart';
 
 class Addexpense extends StatelessWidget {
   const Addexpense({super.key});
@@ -17,8 +18,36 @@ class Addexpense extends StatelessWidget {
           physics: const BouncingScrollPhysics(),
           padding: AppTheme.screenPadding,
           scrollDirection: Axis.vertical,
-          child: Column(children: [
-            
+          child: Column(
+            children: [
+              const SizedBox(height: AppTheme.spacingSM),
+              Container(
+                height: 150,
+                width: double.infinity,
+                padding: AppTheme.cardPadding,
+                decoration: BoxDecoration(
+                  color: AppColors.cardSurface,
+                  borderRadius: BorderRadius.circular(AppDimensions.radiusXL),
+                  border: Border.all(
+                    color: AppColors.border,
+                    width: AppDimensions.borderWidth,
+                  ),
+                  boxShadow: AppTheme.cardShadow,
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Expense Amount', style: AppTheme.h1),
+                    const SizedBox(height: AppTheme.spacingXS),
+                    DynamicTextFormField(
+                      hintText: '1000',
+                      prefixIcon: Icons.currency_rupee_outlined,
+                      suffixIcon: Icons.abc_outlined,
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
